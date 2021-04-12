c4dae1a8fc41c2abfdac62ed34b90d6edf3fa2a3
Miro <mirocosic@gmail.com>
2021-04-12 20:20:15 +0200

    added basic buttons

--- src/app/components/buttons.cljs
+++ src/app/components/buttons.cljs
@@ -1,1 +1,33 @@
-(ns app.ui.components.buttons)
+(ns app.components.buttons
+  (:require  ["react-native" :as rn :refer [Text ActivityIndicator]]
+             ["react-native-gesture-handler" :refer [RectButton]]
+             [app.libs.helix :refer [defnc $]]))
+
+(def default-style {:backgroundColor "cornflowerblue"
+                    :width 150
+                    :height 40
+                    :padding 10
+                    :align-items "center"
+                    :justify-content "center"})
+(def default-label-style {:color "white"})
+(def disabled-style {:backgroundColor "lightgray"})
+(def disabled-label-style {:color "darkgray"})
+
+(defnc LoadingIndicator []
+  ($ ActivityIndicator {:color "white"}))
+
+
+(defnc Primary [{:keys [onPress label loading disabled style label-style]}]
+  ($ RectButton
+     {:onPress (when (and (not loading) (not disabled)) onPress)
+      :enabled (not disabled)
+      :activeOpacity 0.2
+      :style [default-style
+              (when disabled disabled-style)
+              style]}
+     (if loading
+       ($ LoadingIndicator)
+       ($ Text {:style [default-label-style
+                        (when disabled disabled-label-style)
+                        label-style]}
+          label))))
--- src/app/screens/home.cljs
+++ src/app/screens/home.cljs
@@ -2,14 +2,19 @@
   (:require ["react-native" :refer [Text View TouchableOpacity]]
             [app.libs.helix :refer [$ defnc]]
             [app.libs.navigation :refer [navigate]]
+            [app.components.buttons :as buttons]
             [keechma.next.helix.core :refer [with-keechma]]))
 
 (defnc Renderer [props]
  ($ View {:style {:flex 1 :justifyContent "center" :alignItems "center"}} 
      ($ Text {:style {:color "black"}} "Hello from Keechma React Native app home!")
      ($ TouchableOpacity {:onPress #(navigate props "settings")}
-      ($ Text {:style {:padding 10 :backgroundColor "white" :margin 10}} "Go To Settings"))))
-     
+      ($ Text {:style {:padding 10 :backgroundColor "white" :margin 10}} "Go To Settings"))
+
+     ($ buttons/Primary {:label "Primary Button" :onPress #(navigate props "settings") :style {:margin 5}})
+     ($ buttons/Primary {:label "Disabled Button" :onPress #(navigate props "settings") :disabled true :style {:margin 5}})
+     ($ buttons/Primary {:label "Primary Button" :onPress #(navigate props "settings") :style {:margin 5} :loading true})))
+
 
  
 
